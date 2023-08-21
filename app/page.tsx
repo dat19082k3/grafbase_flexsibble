95d0b4f4de2760e4f08dffbc1a5abf6daa89fa7d
Duong Tien Dat <datdt1@zent.vn>
2023-08-21 10:22:14 +0700

    firt commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Home=()=>{
+    return(<section className="flexStart flex-col paddings ms-16">
+        <h1>Categories</h1>
+        <h1>Posts</h1>
+        LoadMore
+    </section>)
+}
+
+export default Home;
